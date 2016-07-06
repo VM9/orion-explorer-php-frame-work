36235856c0c00a526351868486fd15008a85a610
Jose Leonan Carvalho <j.leonancarvalho@gmail.com>
2016-07-06 12:24:37 -0300

    Changing blueprint of source code

--- samples/autoloader.php
+++ samples/autoloader.php
@@ -1,28 +1,27 @@
 <?php
+//Autoloader compatible with psr-0  for example scripts
+$base = dirname(__FILE__) . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR;
+set_include_path("{$base}src" . PATH_SEPARATOR . "{$base}lib" . PATH_SEPARATOR . get_include_path());
 
-set_time_limit(0);
-
-spl_autoload_register(function ($className) {
-    $thisClass = str_replace(__NAMESPACE__ . '\\', '', __CLASS__);
-
-    $baseDir = dirname(__FILE__) . DIRECTORY_SEPARATOR .  ".." . DIRECTORY_SEPARATOR . "lib".DIRECTORY_SEPARATOR; //Your Lib Folder
-
-    if (substr($baseDir, -strlen($thisClass)) === $thisClass) {
-        $baseDir = substr($baseDir, 0, -strlen($thisClass));
+//https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-0.md
+//PSR-0 auto loader
+spl_autoload_register(function ($class) use ($base) {
+    if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
+        $classname = str_replace('\\', '/', $class . '.php');
+    } else {
+        $classname = $class . '.php'; //str_replace('_', DIRECTORY_SEPARATOR, $class . '.php');
     }
 
-    $className = ltrim($className, '\\');
-    $fileName = $baseDir;
-    $namespace = '';
-    if ($lastNsPos = strripos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    $filepath = "{$base}%folder%" . DIRECTORY_SEPARATOR .
+            $classname;
 
-    if (file_exists($fileName)) {
-        require $fileName;
+    $is_module = file_exists(str_replace("%folder%", "src", $filepath));
+    $is_lib = file_exists(str_replace("%folder%", "lib", $filepath));
+
+    if ($is_module || $is_lib) {
+        require_once $classname;
+    } else {
+        die("Class Not Found {$classname} in {$base}");
     }
 });
 
