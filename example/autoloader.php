904deb30fa8e3d0c448f2d2628e16eafc9b6f34d
Jose  Leonan Carvalho <j.leonancarvalho@gmail.com>
2019-01-28 18:11:22 -0200

    #1413
    Dei seguimento no desenvolvimento do formulário de colaboração.
    Pra isso eu abri um item no menu que  se chamará colaborações, aonde terá os  submenu câmeras e  lugares (antes escondido em minhas contas)
    Na primeira tela a listagem, que poderá encaminhar para uma visualização/edição ou inclusão de uma nova colaboração.
    
    No estágio atual eu já finalizei a inclusão, isso ficará armazenado numa coleção do mongo (mais rápido pq não precisa modelar tabela nem instalar no banco de produção)
    Terá uma tela de aprovação (backlog)
    Ne método de aprovação o analista escolherá para qual camada de ponto a câmera irá, o sistema terá 2 camadas uma de câmeras com acesso a internet e umas off-line . (vou por fixo)
    
    Logo após o desenvolvimento da inclusão e métodos de backend de aprovação preciso desenvolver a visualização dessas câmeras , só após ter a visualização das câmeras (incluindo webrtc do kurento) é que vou desenvolver a aprovação da câmera e os métodos de edição da área publica, pode ser que etes não fiquem pontos até quinta mas como são menos importantes vou dar prioridade ao que terá resultado visual primeiro.

--- example/autoloader.php
+++ example/autoloader.php
@@ -5,31 +5,340 @@ $time = $time[1] + $time[0];
 $start = $time;
 ?>
 <style>
+    .file-code-line .blob-line-code {
+        background: #333;
+    }
+
+    .file-code-line .blob-line-nums {
+        background: #292929;
+        border-right: 3px solid #707070;
+        color: #c0c0c0;
+    }
+
+    .file-data .line-numbers {
+        font-size: 14px;
+    }
+
+    .highlight {
+        background: rgb(38,41,40);
+        color: #f8f8f2 ;
+    }
+
+    .highlight .bp {
+        color: #f8f8f2 ;
+    }
+
+    .highlight .c {
+        color: #75715e ;
+    }
+
+    .highlight .c1 {
+        color: #75715e ;
+    }
+
+    .highlight .c1,pre .cm {
+        color: rgb(117,113,94);
+        font-style: normal;
+    }
+
+    .highlight .cm {
+        color: #75715e ;
+    }
+
+    .highlight .cp {
+        color: #75715e ;
+    }
+
+    .highlight .cs {
+        color: #75715e ;
+    }
+
+    .highlight .err {
+        background-color: #1e0010 ;
+        color: #960050;
+    }
+
+    .highlight .ge {
+        font-style: italic ;
+    }
+
+    .highlight .gs {
+        font-weight: bold ;
+    }
+
+    .highlight .hll {
+        background-color: #49483e ;
+    }
+
+    .highlight .il {
+        color: #ae81ff ;
+    }
+
+    .highlight .k {
+        color: #66d9ef ;
+    }
+
+    .highlight .k ,.highlight .nt,.highlight .kd {
+        color: rgb(249,38,114);
+        font-weight: normal;
+    }
+
+    .highlight .kc {
+        color: #66d9ef ;
+    }
+
+    .highlight .kd {
+        color: #66d9ef ;
+    }
+
+    .highlight .kn {
+        color: #f92672 ;
+    }
+
+    .highlight .kp {
+        color: #66d9ef ;
+    }
+
+    .highlight .kr {
+        color: #66d9ef ;
+    }
+
+    .highlight .kt {
+        color: #66d9ef ;
+    }
+
+    .highlight .l {
+        color: #ae81ff ;
+    }
+
+    .highlight .ld {
+        color: #e6db74 ;
+    }
+
+    .highlight .m {
+        color: #ae81ff ;
+    }
+
+    .highlight .mf {
+        color: #ae81ff ;
+    }
+
+    .highlight .mh {
+        color: #ae81ff ;
+    }
+
+    .highlight .mi {
+        color: #ae81ff ;
+    }
+
+    .highlight .mo {
+        color: #ae81ff ;
+    }
+
+    .highlight .n {
+        color: #f8f8f2 ;
+    }
+
+    .highlight .na {
+        color: #a6e22e ;
+    }
+
+    .highlight .nb {
+        color: #f8f8f2 ;
+    }
+
+    .highlight .nc {
+        color: #a6e22e ;
+    }
+
+    .highlight .nd {
+        color: #a6e22e ;
+    }
+
+    .highlight .ne {
+        color: #a6e22e ;
+    }
+
+    .highlight .nf {
+        color: #a6e22e ;
+    }
+
+    .highlight .ni {
+        color: #f8f8f2 ;
+    }
+
+    .highlight .nl {
+        color: #f8f8f2 ;
+    }
+
+    .highlight .nn {
+        color: #f8f8f2 ;
+    }
+
+    .highlight .no {
+        color: #66d9ef ;
+    }
+
+    .highlight .nt {
+        color: #f92672 ;
+    }
+
+    .highlight .nv {
+        color: #f8f8f2 ;
+    }
+
+    .highlight .nv,.highlight .na {
+        color: rgb(166,226,46);
+    }
+
+    .highlight .nx {
+        color: #a6e22e ;
+    }
+
+    .highlight .o {
+        color: #f92672 ;
+    }
+
+    .highlight .ow {
+        color: #f92672 ;
+    }
+
+    .highlight .p {
+        color: #f8f8f2 ;
+    }
+
+    .highlight .py {
+        color: #f8f8f2 ;
+    }
+
+    .highlight .s {
+        color: #e6db74 ;
+    }
+
+    .highlight .s,.highlight .s2, .highlight .s1 {
+        color: rgb(229,218,91)
+    ;
+    }
+
+    .highlight .s1 {
+        color: #e6db74 ;
+    }
+
+    .highlight .s2 {
+        color: #e6db74 ;
+    }
+
+    .highlight .sb {
+        color: #e6db74 ;
+    }
+
+    .highlight .sc {
+        color: #e6db74 ;
+    }
+
+    .highlight .sd {
+        color: #e6db74 ;
+    }
+
+    .highlight .se {
+        color: #ae81ff ;
+    }
+
+    .highlight .sh {
+        color: #e6db74 ;
+    }
+
+    .highlight .si {
+        color: #e6db74 ;
+    }
+
+    .highlight .sr {
+        color: #e6db74 ;
+    }
+
+    .highlight .ss {
+        color: #e6db74 ;
+    }
+
+    .highlight .sx {
+        color: #e6db74 ;
+    }
+
+    .highlight .vc {
+        color: #f8f8f2 ;
+    }
+
+    .highlight .vg {
+        color: #f8f8f2 ;
+    }
+
+    .highlight .vi {
+        color: #f8f8f2 ;
+    }
+
+    .highlight .w {
+        color: #f8f8f2 ;
+    }
+
+    .highlight pre {
+        background: transparent;
+        color: #f8f8f2 ;
+    }
+
+    .highlight pre span {
+        font-weight: normal;
+    }
+
+    .markdown-body pre {
+        border: 0;
+        border-radius: 0;
+        padding-left: 10px;
+    }
+
+    div.code-body.highlight,pre,.data.highlight {
+        background: rgb(38,41,40);
+        color: #ddd;
+        font-family: 'Monaco',"Consolas";
+        font-size: 16px;
+    }
+
+    .file-code {
+        background: rgb(38,41,40);
+    }
+
+    .highlight .gd .diff-line-code,.highlight .gd .diff-line-num {
+        background: #a00;
+    }
+
+    .highlight .gi .diff-line-code,.highlight .gi .diff-line-num {
+        background: #006A74;
+    }
+
+    .highlight .gc .diff-line-code,.highlight .gc .diff-line-num {
+        background: #948200;
+    }
+
+    .highlight .gi .x {
+        background: #006A74;
+        color: #fff;
+    }
+
+    .file-diff-line:hover .diff-line-code,.file-diff-line:hover .diff-line-num {
+        background: #000;
+    }
+
     pre {
-        display:block;
-        font:normal 12px/22px Monaco,Monospace !important;
-        color:#CFDBEC;
-        background-color:#272821;
-        background-image:-webkit-repeating-linear-gradient(top, #444 0px, #444 22px, #272821 22px, #272821 44px);
-        background-image:-moz-repeating-linear-gradient(top, #444 0px, #444 22px, #272821 22px, #272821 44px);
-        background-image:-ms-repeating-linear-gradient(top, #444 0px, #444 22px, #272821 22px, #272821 44px);
-        background-image:-o-repeating-linear-gradient(top, #444 0px, #444 22px, #272821 22px, #272821 44px);
-        background-image:repeating-linear-gradient(top, #444 0px, #444 22px, #272821 22px, #272821 44px);
-        padding:0em 1em;
-        overflow:auto;
+        font-family: "Ayuthaya","Lucida Sans Typewriter","Menlo","Monospaced","Courier","Andale Mono";
+        font-size: 14px;
+        font-weight: normal;
+        line-height: 1.5em;
     }
-    code{
-        display:block;
-        font:normal 12px/22px Monaco,Monospace !important;
-        background-color:#676360;
-        background-image:-webkit-repeating-linear-gradient(top, #272821 0px, #272821 22px, #272822 22px, #272822 44px);
-        background-image:-moz-repeating-linear-gradient(top, #272821 0px, #272821 22px, #272822 22px, #272822 44px);
-        background-image:-ms-repeating-linear-gradient(top, #272821 0px, #272821 22px, #272822 22px, #272822 44px);
-        background-image:-o-repeating-linear-gradient(top, #272821 0px, #272821 22px, #272822 22px, #272822 44px);
-        background-image:repeating-linear-gradient(top, #272821 0px, #272821 22px, #272822 22px, #272822 44px);
-        padding:0em 1em;
-        overflow:auto;
+
+    pre code {
+        font-size: 14px;
     }
+
+
 </style>
 <a href="../example">Back</a>
 <?php
@@ -70,4 +379,4 @@ function getRandLng() {
     return rand(-178, 179) + (mt_rand() / 10000000000);
 }
 
-$ip = "192.168.1.117";
+$ip = "ngsi.vm9it.com";
